dc49b6992101b9a4e194c3bcac09929d9007035a
sozib62 <webdevsozib62@gmail.com>
2022-10-25 19:00:30 +0600

    course caterogy data rdy

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courseCategory = require('./Data/courseCategory.json')
+
 app.get('/', (req, res) => {
     res.send('Education server running')
 });
+app.get('/category', (req, res) => {
+    res.send(courseCategory)
+});
+
+
 
 app.listen(port, () => {
     console.log(`Education server running on port: ${port}`)

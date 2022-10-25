5c27ebcdc8b405988ddc5b9e42c8ad3ea9456e32
sozib62 <webdevsozib62@gmail.com>
2022-10-26 00:40:35 +0600

    added cors

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
 
 const courseCategory = require('./Data/courseCategory.json')
 
 app.get('/', (req, res) => {
     res.send('Education server running')
 });
+
 app.get('/category', (req, res) => {
     res.send(courseCategory)
 });
-
-
 
 app.listen(port, () => {
     console.log(`Education server running on port: ${port}`)
